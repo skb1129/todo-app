7e5485c43850cc7e7d719bddf6071b0bef212831
Surya Kant Bansal <suryakantbansal97@gmail.com>
2020-08-29 22:52:59 +0530

    setup heroku deploy

--- database/config.py
+++ database/config.py
@@ -1,11 +1,9 @@
 import os
 
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(32))
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
